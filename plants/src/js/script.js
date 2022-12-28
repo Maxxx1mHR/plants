b6fc85389fc36a4b55ff6cc069f376f0ad6e38cd
Maxim <maksim.pudeev@bk.ru>
2022-12-28 16:03:42 +0300

    fix: accordion prices section. fix: check pixel perfect for desktop

--- plants/src/js/script.js
+++ plants/src/js/script.js
@@ -19,8 +19,27 @@
 // });
 
 
+// document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader) => {  //получаю все header на которые буду нажимать
+//     accordionItemHeader.addEventListener('click', () => {
+//         accordionItemHeader.classList.toggle('active'); //добавляю класс активности для header + <-> -
+//         const accordionItemBody = accordionItemHeader.nextElementSibling; //получаю следующий элемент за header
+//         if(accordionItemHeader.classList.contains('active')) {    
+//             //если у header есть класс активности, тогда выставляем max-height
+//             accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+//         }
+//         else {
+//             accordionItemBody.style.maxHeight = 0;
+//         }
+//     });
+// });
+
+
 document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader) => {  //получаю все header на которые буду нажимать
-    accordionItemHeader.addEventListener('click', () => {
+    accordionItemHeader.addEventListener('click', (evt) => {
+        console.log(evt.target);
+        const elai = evt.target.closest('.accordion-item');
+        console.log(elai);
+        elai.classList.toggle('active');
         accordionItemHeader.classList.toggle('active'); //добавляю класс активности для header + <-> -
         const accordionItemBody = accordionItemHeader.nextElementSibling; //получаю следующий элемент за header
         if(accordionItemHeader.classList.contains('active')) {    
