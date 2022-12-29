ead49760e46c39daba2aa57ad67d4c00c9c08450
Maxim <maksim.pudeev@bk.ru>
2022-12-29 16:06:11 +0300

    feat: add contacts__accordion

--- plants/src/js/script.js
+++ plants/src/js/script.js
@@ -36,18 +36,22 @@
 
 document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader) => {  //получаю все header на которые буду нажимать
     accordionItemHeader.addEventListener('click', (evt) => {
-        console.log(evt.target);
-        const elai = evt.target.closest('.accordion-item');
-        console.log(elai);
-        elai.classList.toggle('active');
+        // console.log(evt.target);
+        // const elai = evt.target.closest('.accordion-item');
+        // console.log(elai);
+        // elai.classList.toggle('active');
         accordionItemHeader.classList.toggle('active'); //добавляю класс активности для header + <-> -
         const accordionItemBody = accordionItemHeader.nextElementSibling; //получаю следующий элемент за header
+        const accordionItem = accordionItemHeader.parentElement;
+        console.log(accordionItem); 
         if(accordionItemHeader.classList.contains('active')) {    
             //если у header есть класс активности, тогда выставляем max-height
             accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+            accordionItem.classList.add('active');
         }
         else {
             accordionItemBody.style.maxHeight = 0;
+            accordionItem.classList.remove('active');
         }
     });
 });
