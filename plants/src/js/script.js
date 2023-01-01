23496f6a02d3b234c5d42e405f57d955186628a0
Your Name <you@example.com>
2023-01-01 20:06:08 +0300

    fix: contacts__accordion

--- plants/src/js/script.js
+++ plants/src/js/script.js
@@ -56,33 +56,54 @@ document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader
     });
 });
 
+
 //Contacts
+
 document.querySelectorAll('.contacts__accordion-header').forEach((accordionHeader) => {
-    accordionHeader.addEventListener('click', (event) => {
-        accordionHeader.classList.toggle('active');
+    accordionHeader.addEventListener('click', () => {
+        accordionHeader.classList.toggle('active-bgc');
+
         const accordionWrapper = accordionHeader.nextElementSibling; //получаю следующий элемент за header -> contacts__wrapper
         // accordionWrapper.classList.toggle('active');  //так работает, но не получается сделать плавное раскрытие
-        if(accordionHeader.classList.contains('active')) {
+        if(accordionHeader.classList.contains('active-bgc')) {
             accordionWrapper.style.maxHeight = accordionWrapper.scrollHeight + 'px';
+            accordionHeader.classList.add('active-arrow');
         }
         else {
             accordionWrapper.style.maxHeight = 0;
+            accordionHeader.classList.remove('active-arrow');
         }
-        
-        document.querySelectorAll('.contacts__accordion-city').forEach((accordionCity) => {
-            accordionCity.addEventListener('click', (event) => {
-                let content = event.target.innerHTML;
-                // console.log(content);
-               accordionHeader.innerHTML = content;
-            })
-        })
+    });
+});
+
+const accordionHeader = document.querySelector('.contacts__accordion-header');
+document.querySelectorAll('.contacts__accordion-city').forEach((accordionCity) => {
+    accordionCity.addEventListener('click', (event) => {
+        let content = event.target.innerHTML;
+       accordionHeader.innerHTML = content;
+       accordionHeader.classList.remove('active-bgc'); 
+       accordionHeader.classList.add('bgc_green'); 
+       accordionHeader.classList.remove('active-arrow');    
+       const accordionWrapper = accordionHeader.nextElementSibling;
+       accordionWrapper.style.maxHeight = 0;
+    });
+});
 
 
 
+const test  = document.querySelectorAll('.contacts__info');
+document.querySelectorAll('.contacts__accordion-city').forEach((accordionCity) => {
+    accordionCity.addEventListener('click', () => {
+        let tabId = accordionCity.getAttribute('data-tab');//получаю tab_n , n =1,2,3,4 при клике
+        console.log(tabId);
+        let currentTab = document.querySelector(tabId);
 
+        test.forEach((item) => {
+            item.classList.remove('active');
+        });
+       currentTab.classList.add('active');
 
-    }) 
-})
-
+    });
+});
 
 
