6f2a68f4ee29d265dd8087d8c357b7f01ee079e7
Your Name <you@example.com>
2023-01-02 16:58:32 +0300

    feat: add blur filter for service section. delete comments from js file

--- plants/src/js/script.js
+++ plants/src/js/script.js
@@ -1,37 +1,26 @@
-// const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+//Service 
+const btns = document.querySelectorAll('.service__btns-btn');
+const serviceBlocks = document.querySelectorAll('.service__block');
 
+btns.forEach((item) => {
+    item.addEventListener('click', () => {
+        let btn = item.getAttribute('data-blur');
+        let currentBlocks = document.querySelectorAll(btn);
+        serviceBlocks.forEach((event) => {
+            event.classList.add('filter-blur');
+        });
+        currentBlocks.forEach((event) => {
+            event.classList.remove('filter-blur');
+        });
 
-// accordionItemHeaders.forEach(accordionItemHeader => {
-//     accordionItemHeader.addEventListener("click", event => {
+        let currentBtn = document.querySelector(`[data-blur='${btn}']`);
+        btns.forEach((item) => {
+            item.classList.remove('bgc_orange');
+        });
+        currentBtn.classList.add('bgc_orange');
+    }); 
+});
 
-//         accordionItemHeader.classList.toggle("active");
-
-//         const accordionItemBody = accordionItemHeader.nextElementSibling;
-       
-
-//         if(accordionItemHeader.classList.contains("active")) {
-//             accordionItemBody.classList.add("active");
-//         }
-//         else {
-//             accordionItemBody.classList.remove("active");
-//         }
-//     });
-// });
-
-
-// document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader) => {  //получаю все header на которые буду нажимать
-//     accordionItemHeader.addEventListener('click', () => {
-//         accordionItemHeader.classList.toggle('active'); //добавляю класс активности для header + <-> -
-//         const accordionItemBody = accordionItemHeader.nextElementSibling; //получаю следующий элемент за header
-//         if(accordionItemHeader.classList.contains('active')) {    
-//             //если у header есть класс активности, тогда выставляем max-height
-//             accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
-//         }
-//         else {
-//             accordionItemBody.style.maxHeight = 0;
-//         }
-//     });
-// });
 
 //Prices 
 document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader) => {  //получаю все header на которые буду нажимать
@@ -55,58 +44,6 @@ document.querySelectorAll('.accordion-item-header').forEach((accordionItemHeader
         }
     });
 });
-
-
-//Contacts
-
-// document.querySelectorAll('.contacts__accordion-header').forEach((accordionHeader) => {
-//     accordionHeader.addEventListener('click', () => {
-//         accordionHeader.classList.toggle('active-bgc');
-
-//         const accordionWrapper = accordionHeader.nextElementSibling; //получаю следующий элемент за header -> contacts__wrapper
-//         // accordionWrapper.classList.toggle('active');  //так работает, но не получается сделать плавное раскрытие
-//         if(accordionHeader.classList.contains('active-bgc')) {
-//             accordionWrapper.style.maxHeight = accordionWrapper.scrollHeight + 'px';
-//             accordionHeader.classList.add('active-arrow');
-//         }
-//         else {
-//             accordionWrapper.style.maxHeight = 0;
-//             accordionHeader.classList.remove('active-arrow');
-//         }
-//     });
-// });
-
-// const accordionHeader = document.querySelector('.contacts__accordion-header');
-// document.querySelectorAll('.contacts__accordion-city').forEach((accordionCity) => {
-//     accordionCity.addEventListener('click', (event) => {
-//         let content = event.target.innerHTML;
-//        accordionHeader.innerHTML = content;
-//        accordionHeader.classList.remove('active-bgc'); 
-//        accordionHeader.classList.add('bgc_green'); 
-//        accordionHeader.classList.remove('active-arrow');    
-//        const accordionWrapper = accordionHeader.nextElementSibling;
-//        accordionWrapper.style.maxHeight = 0;
-//     });
-// });
-
-
-
-// const test  = document.querySelectorAll('.contacts__info');
-// document.querySelectorAll('.contacts__accordion-city').forEach((accordionCity) => {
-//     accordionCity.addEventListener('click', () => {
-//         let tabId = accordionCity.getAttribute('data-tab');//получаю tab_n , n =1,2,3,4 при клике
-//         console.log(tabId);
-//         let currentTab = document.querySelector(tabId);
-
-//         test.forEach((item) => {
-//             item.classList.remove('active');
-//         });
-//        currentTab.classList.add('active');
-
-//     });
-// });
-
-
 
 //Contacts
 const accordionHeader = document.querySelector('.contacts__accordion-header');
